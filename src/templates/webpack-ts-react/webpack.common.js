c2b2a5ad4680d2c4ecca449aaa4e90380d207a3a
jiangsongyang <2744856707@qq.com>
2022-08-22 17:40:59 +0800

    [feat] : update webpack config

--- src/templates/webpack-ts-react/webpack.common.js
+++ src/templates/webpack-ts-react/webpack.common.js
@@ -1,196 +1,109 @@
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ESLintPlugin = require('eslint-webpack-plugin')
+import { join, resolve } from 'node:path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ESLintPlugin from 'eslint-webpack-plugin'
+import { ProgressPlugin } from 'webpack'
+import { CWD } from '../shared/constant'
 
-const __DEV__ = process.env.NODE_ENV === 'development'
+const NODE_MODULES_PATH = resolve(CWD, '../cli/node_modules')
 
-module.exports = {
-  stats: {
-    chunks: false,
-    children: false,
-    modules: false,
-    colors: true,
-  },
-  devtool: __DEV__ ? 'source-map' : false,
-  entry: './src/main.tsx',
+export const WEBPACK_COMMON_CONFIG= {
+  entry: resolve(CWD, 'src/main.tsx'),
   output: {
-    libraryTarget: 'umd',
-    filename: '[name].[hash].js',
-    chunkFilename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    path: resolve(CWD, 'dist'),
     clean: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', 'json'],
-    alias: {
-      '@src': path.join(__dirname, 'src'),
-      '@components': path.join(__dirname, './src/components'),
-      '@shared': path.join(__dirname, './shared'),
-      '@api': path.join(__dirname, './api'),
-    },
+    publicPath: '/assets-se',
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.css$/i,
+        use: [join(NODE_MODULES_PATH, 'style-loader'), join(NODE_MODULES_PATH, 'css-loader')],
+      },
+      {
+        test: /\.less$/i,
         use: [
+          // compiles Less to CSS
+          join(NODE_MODULES_PATH, 'style-loader'),
+          join(NODE_MODULES_PATH, 'css-loader'),
           {
-            loader: MiniCssExtractPlugin.loader,
+            loader: join(NODE_MODULES_PATH, 'less-loader'), // compiles Less to CSS
             options: {
-              esModule: true,
-            },
-          },
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
+              lessOptions: {
+                javascriptEnabled: true,
+              },
             },
           },
         ],
       },
       {
-        test: /\.less$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              esModule: true,
-            },
-          },
-          { loader: 'css-loader' },
-          {
-            loader: 'less-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(tsx|jsx)$/,
+        test: /\.(tsx)?$/,
         exclude: /node_modules/,
+        enforce: 'pre',
         use: [
           {
-            loader: 'babel-loader',
+            loader: join(NODE_MODULES_PATH, 'babel-loader'),
             options: {
               presets: [
-                ['@babel/preset-typescript', { allowNamespaces: true }],
-                ['@babel/preset-react', { runtime: 'automatic' }],
+                join(NODE_MODULES_PATH, '@babel/preset-env'),
+                join(NODE_MODULES_PATH, '@babel/preset-react'),
                 [
-                  '@babel/preset-env',
+                  join(NODE_MODULES_PATH, '@babel/preset-typescript'),
+                  { isTSX: true, allExtensions: true },
+                ],
+              ],
+              // 缓存：第二次构建时，会读取之前的缓存
+              cacheDirectory: true,
+              plugins: [
+                [join(NODE_MODULES_PATH, 'babel-plugin-react-require')],
+                [
+                  join(NODE_MODULES_PATH, 'babel-plugin-import'),
                   {
-                    targets: { chrome: '63' },
-                    loose: true,
+                    libraryName: 'antd',
+                    style: true, // or 'css'
                   },
                 ],
               ],
-              plugins: [
-                'jsx-control-statements',
-                ['@babel/plugin-syntax-dynamic-import'],
-                ['@babel/plugin-proposal-decorators', { legacy: true }],
-                ['@babel/plugin-proposal-class-properties', { loose: true }],
-                '@babel/plugin-proposal-object-rest-spread',
-                '@babel/plugin-proposal-optional-chaining',
-                '@babel/plugin-proposal-nullish-coalescing-operator',
-                '@babel/plugin-transform-react-constant-elements',
-              ],
             },
           },
         ],
       },
       {
-        test: /\.(ts|js)$/,
-        exclude: /node_modules/,
+        test: /\.(ts)?$/,
+        // include: [resolve('src'), resolve('api')],
+        exclude(modulePath) {
+          return /node_modules/.test(modulePath) && !/\/common\//.test(modulePath)
+        },
         use: [
           {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-typescript', { allowNamespaces: true }],
-                '@babel/preset-react',
-                [
-                  '@babel/preset-env',
-                  {
-                    targets: { chrome: '63' },
-                    loose: true,
-                  },
-                ],
-              ],
-              plugins: [
-                'jsx-control-statements',
-                ['@babel/plugin-syntax-dynamic-import'],
-                ['@babel/plugin-proposal-decorators', { legacy: true }],
-                ['@babel/plugin-proposal-class-properties', { loose: true }],
-                '@babel/plugin-proposal-object-rest-spread',
-                '@babel/plugin-proposal-optional-chaining',
-                '@babel/plugin-proposal-nullish-coalescing-operator',
-                '@babel/plugin-transform-react-constant-elements',
-              ],
-            },
+            loader: join(NODE_MODULES_PATH, 'ts-loader'),
           },
         ],
-      },
-      {
-        test: /\.(jpe?g|png|gif|ico|webp)$/,
-        use: [
-          {
-            loader: require.resolve('url-loader'),
-            options: {
-              limit: 8192,
-              fallback: require.resolve('file-loader'),
-              name: '[name]-[hash].[ext]',
-              esModule: false,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(svg)$/,
-        loader: 'svg-react-loader',
       },
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      hash: false,
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-      chunkFilename: '[id].[contenthash:12].css',
-      ignoreOrder: false,
+    new ProgressPlugin({
+      activeModules: false,
+      entries: true,
+      modules: true,
+      modulesCount: 5000,
+      profile: false,
+      dependencies: true,
+      dependenciesCount: 10000,
+      percentBy: 'entries',
     }),
     new ESLintPlugin({
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      fix: true /* 自动帮助修复 */,
+      extensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'react'],
+      exclude: 'node_modules',
+    }),
+    new HtmlWebpackPlugin({
+      template: resolve(CWD, 'public/index.html'),
     }),
   ],
-  optimization: {
-    runtimeChunk: {
-      name: 'manifest',
-    },
-    splitChunks: {
-      chunks: 'all',
-      minSize: 30000,
-      cacheGroups: {
-        default: {
-          name: 'bundle',
-          priority: -30,
-        },
-        common: {
-          test: /[\\/]src\/components[\\/]/,
-          name: 'common',
-          priority: -20,
-        },
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          priority: -10,
-        },
-      },
-    },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
   },
+  externals: [],
 }
